95a276cb9716115b8a497a8709ed930260f7dd74
unknown <singhabhishekj21@gmail.com>
2023-10-01 18:48:50 +0100

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <Toaster />
   </BrowserRouter>
 );
